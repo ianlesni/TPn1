dc926e3ec1d6c863d61625680729b83fea7beb7c
ianlesni <ianlesni@gmail.com>
2024-07-13 19:23:33 +0000

    Estructuro archivos

---
+++ modules/midi_uart/midi_uart.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _MIDI_UART_H_
+#define _MIDI_UART_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _MIDI_UART_H_
---
+++ modules/piezo/piezo.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _PIEZO_H_
+#define _PIEZO_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _PIEZO_H_
