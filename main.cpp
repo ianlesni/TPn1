9aa8150a2fd4ee29d52ab6c71668d8c88e14ac2e
ianlesni <ianlesni@gmail.com>
2024-07-20 04:30:59 +0000

    Correcciones de formato y funcion de interfaz visual

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
- /*************************************************
- *=====[Bibliotecas]===============================
- *************************************************/
+//=====[Libraries]=============================================================
+
 #include "mbed.h" 
 #include "display.h"
 #include "midi_uart.h"
@@ -15,129 +14,113 @@
 #include "arm_book_lib.h"
 #include <cstdint>
 
- /*************************************************
- *=====[Definiciones]==============================
- *************************************************/
+//=====[Declaration of defines]================================================
 
- /*******************************************************************
- *=====[Declaración e inicialización de objetos globales]============
- *******************************************************************/
+//=====[Declaration and initialization of public global objects]===============
 
-DigitalOut ledPad(LED1);                                    //Creo un objeto DigitalOut como led testigo de interacción con el drum pad
-  
-/********************************************************************
- *=======[Declaración e inicialización de variables globales]========
- ********************************************************************/
+//=====[Declaration of  public global variables]===============================
 
-/*!
- * \enum LED_STATE
- * \brief Enumeración de los estados de los leds.
- *
- */
-typedef enum{
+int8_t noteIndex = 0;        /**< Indice para la navegación del arreglo de notas de intrumento */
 
-    LED_ON = 1,     /**< Led encendido */
-    LED_OFF = 0     /**< Led apagado */
-
-} LED_STATE; 
-
-/*********************************************************************
- *=====[Declaración (prototipos) de funciones públicas]===============
- ********************************************************************/
+//=====[Declarations (prototypes) of public functions]=========================
 
 /**
  * Inicialización los dispositivos de salida.
  * 
  * Esta función inicializa el led y el display del drum pad.
  */
-void visualInterfaceInit (void);
+void visualInterfaceInit (DigitalOut * Led);
 
-/*************************************************************************
- *======================[Función main]====================================
- ************************************************************************/
-int8_t noteIndex = 0;  
-
+//=====[Main function]=========================================================
 int main(void)
 {
+    DigitalOut ledPad(LED1);                                        //Creo un objeto DigitalOut como led testigo de interacción con el drum pad
+    
     /** Creo el transductor piezo eléctrico  
     */
     AnalogIn piezoA(A0);
     piezo_t piezoAStruct;
     piezoInit(&piezoA, &piezoAStruct);
+
     /** Creo los pulsadores necesarios para configurar el 
-    *  sonido del drum pad. 
-    *  Estos pulsadores permiten navegar de manera ascendente y 
-    *  descendente por el arreglo de notas de instrumentos
-    *  disponibles.
+    *   sonido del drum pad    
     */
-    DigitalIn upButton(BUTTON1);                                                //Creo un objeto DigitalIn para la navegación ascendente del arreglo de notas midi disponibles
+    DigitalIn upButton(BUTTON1);                                                
     button_t  upButtonStruct;
     upButtonStruct.alias = &upButton;
     debounceButtonInit(&upButtonStruct);
 
-    DigitalIn downButton(D1);                                                   //Creo un objeto DigitalIn para la navegación descendente del arreglo de notas midi disponibles
+    DigitalIn downButton(D1);                                                   
     button_t  downButtonStruct;
     downButtonStruct.alias = &downButton;
     debounceButtonInit(&downButtonStruct);
 
-    UnbufferedSerial serialPort(USBTX, USBRX);   //Creo un objeto UnbufferedSerial para realizar la comunicación serie con la PC.
+    /** Creo objeto UnbufferedSerial para realizar
+    *   la comunicación serie con la PC   
+    */
+    UnbufferedSerial serialPort(USBTX, USBRX);                          
     midiMessage_t midiMessageStruct; 
     initializaMIDISerial(&serialPort, &midiMessageStruct);
 
-    visualInterfaceInit();                                              //Inicializo el led del drum pad
+    visualInterfaceInit(&ledPad);                                       //Inicializo el led del drum pad y display
        
     uint8_t numOfInstrumentNotes = getNumOfInstrumentNotes();           //Obtengo el número total de notas midi de instrumentos percusivos disponibles
-                                            /**< Indice para la navegación del arreglo de notas de intrumento */
+                                            
 
     while (true)
     {
+        /** Actualizo la maquina de estados
+        *   que gestiona el debounce de los
+        *   pulsadores   
+        */
         bool downButtonReleased = debounceButtonUpdate(&downButtonStruct);
         bool upbuttonReleased = debounceButtonUpdate(&upButtonStruct);
 
-        if(PIEZO_ACTIVE == piezoUpdate(&piezoAStruct))                  //Actualizo y verifico el estado del transductor piezoeléctrico
+        if(PIEZO_ACTIVE == piezoUpdate(&piezoAStruct))                          //Actualizo y verifico el estado del transductor piezoeléctrico
         {  
-            ledPad = LED_ON;                                            //Enciendo el Led para confirmar que se realizó un golpe que superó el umbral de activación
-            midiSendNoteOff(&midiMessageStruct, &serialPort);           //Envío el mensaje de Note Off para no superponer notas
-            midiMessageStruct.note = instrumentNote[noteIndex];         //Cargo la nota del mensaje
-            midiMessageStruct.velocity = piezoAStruct.MaxVelocity;      //Cargo la velocity del mensaje               
-            midiSendNoteOn(&midiMessageStruct, &serialPort);            //Envío el mensaje de Note On con el parámetro velocity proporcional a la intensidad del golpe
-            ledPad = LED_OFF;                                           //Apago el Led para indicar que se envió el mensaje correspondiente
+            ledPad = 1;                                                         //Enciendo el Led para confirmar que se realizó un golpe que superó el umbral de activación
+            midiSendNoteOff(&midiMessageStruct, &serialPort);                   //Envío el mensaje de Note Off para no superponer notas
+            midiMessageStruct.note = instrumentNote[noteIndex];                 //Cargo la nota del mensaje
+            midiMessageStruct.velocity = piezoAStruct.MaxVelocity;              //Cargo la velocity del mensaje               
+            midiSendNoteOn(&midiMessageStruct, &serialPort);                    //Envío el mensaje de Note On con el parámetro velocity proporcional a la intensidad del golpe
+            ledPad = 0;                                                         //Apago el Led para indicar que se envió el mensaje correspondiente
         }
 
-        if(true == upbuttonReleased)                                    //Verifico si el pulsador upButton fué presionado
+        if(true == upbuttonReleased)                                            //Verifico si el pulsador upButton fué presionado
         {
-            noteIndex++;                                                //Incremento el indice de navegación de notas
-            if (noteIndex >= numOfInstrumentNotes) noteIndex = 0;       //Controlo que el indice no se vaya de rango
+            noteIndex++;                                                        //Incremento el indice de navegación de notas
+            if (noteIndex >= numOfInstrumentNotes) noteIndex = 0;               //Controlo que el indice no se vaya de rango
             displayCharPositionWrite (0,1);
             displayStringWrite("                ");
             displayCharPositionWrite (0,1);
             displayStringWrite(instrumentNoteName[noteIndex]);
         }
 
-        if(true == downButtonReleased)                                    //Verifico si el pulsador downButton fué presionado
+        if(true == downButtonReleased)                                          //Verifico si el pulsador downButton fué presionado
         {
-            noteIndex--;                                                //Decremento el indice de navegación de notas
-            if (noteIndex < 0) noteIndex = numOfInstrumentNotes - 1;    //Controlo que el indice no se vaya de rango
+            noteIndex--;                                                        //Decremento el indice de navegación de notas
+            if (noteIndex < 0) noteIndex = numOfInstrumentNotes - 1;            //Controlo que el indice no se vaya de rango
             displayCharPositionWrite (0,1);
             displayStringWrite("                ");
             displayCharPositionWrite (0,1);
             displayStringWrite(instrumentNoteName[noteIndex]);
         }
+        
         delay(TIME_INCREMENT_MS);
     }
 
 }
 
-/*********************************************************************
- *=====[Implementación de funciones públicas]=========================
- ********************************************************************/
 
-void visualInterfaceInit ()
+//=====[Implementations of public functions]===================================
+void visualInterfaceInit(DigitalOut * Led)
 {
-    ledPad = LED_OFF;   //Inicializo el led del drum pad apagado
+    Led->write(0);                                                  //Inicializo el led del drum pad apagado
+
     displayInit(DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER);
     displayCharPositionWrite(0,0);
     displayStringWrite("MIDI Drum Pad v0");
+    
     delay(550);
 
     displayCharPositionWrite(0,0);
